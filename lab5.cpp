47593f4f83b4ce701cb5876cd430e9d867315356
Ermekova Dayana <ermekova.dayana09@mail.ru>
2023-11-24 22:36:14 +0500

    modified:   lab5.cpp

--- lab5.cpp
+++ lab5.cpp
@@ -1,4 +1,8 @@
-﻿#include <iostream>
+﻿/*Программа является учебной
+	автор: Ермекова Даяна
+	группа: КЭ-217
+	год: 2023*/
+#include <iostream>
 using namespace std;
 int main()
 {
@@ -10,5 +14,6 @@ int main()
 		cin >> y;
 		cout <<x<<"+"<<y<<"="<< x + y << endl;
 		cout << x << "-" << y << "=" << x - y << endl;
+		cout << x << "*" << y << "=" << x * y << endl;
 		return 0;
 }
